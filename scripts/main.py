918db355ca469577a5a76bf4b006eb64c9908cc0
WillSho1 <whshocon@gmail.com>
2025-04-24 12:44:48 -0400

    clean and improve documentation

--- scripts/main.py
+++ scripts/main.py
@@ -9,38 +9,31 @@ from sklearn.model_selection import cross_val_score
 def train_model_on_label(X_train, train_data, X_test, test_data, label):
     mnb = MultinomialNB(alpha=1.0)
     print(f"Training MNB model on label: {label.upper()}...")
+    # train
     mnb.fit(X_train, train_data[label])
 
-    #Train cross-validation
+    # cross-validation on train data
+    # split train into chunks and train each chunk and evaluate on the rest
+    # new models for each chunk - does not use model trained above
     print("Cross-validation on train data...")
     cv_scores = cross_val_score(mnb, X_train, train_data[label], cv=5, scoring='accuracy')
     print(f"Train CV Accuracy: {cv_scores.mean():.2f} ± {cv_scores.std():.2f}")
 
-    #Test predictions
+    # actual model used here
+    # predictions on test data
     print("Predicting on test data...")
     predictions = mnb.predict(X_test)
-    true_labels = test_data[label].values
 
-    #Save predictions
-    # output_dir = "data/predictions"
-    # #os.makedirs(output_dir, exist_ok=True)
-    # np.savez_compressed(f"{output_dir}/{label}_predictions.npz", y_true=true_labels, y_pred=predictions)
-
-    # Evaluation metrics
-    acc = accuracy_score(true_labels, predictions)
-    prec = precision_score(true_labels, predictions, zero_division=0)
-    rec = recall_score(true_labels, predictions, zero_division=0)
-    f1 = f1_score(true_labels, predictions, zero_division=0)
-
+    # evaluation
+    acc = accuracy_score(test_data[label], predictions)
+    prec = precision_score(test_data[label], predictions, zero_division=0)
+    rec = recall_score(test_data[label], predictions, zero_division=0)
+    f1 = f1_score(test_data[label], predictions, zero_division=0)
     print(f"\n📊 Evaluation Metrics for '{label}':")
     print(f"Accuracy : {acc:.4f}")
     print(f"Precision: {prec:.4f}")
     print(f"Recall   : {rec:.4f}")
     print(f"F1 Score : {f1:.4f}\n")
-
-    #Optional: Full classification report
-    #print("Classification Report:")
-    #print(classification_report(true_labels, predictions))
 
 if __name__ == '__main__':
     # load data
